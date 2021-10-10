4039141356e2116a234d9a583c041d70e5030760
Rakshith <rakshithhgd@gmail.com>
2021-10-10 22:36:46 +0530

    banner-created

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,7 +1,20 @@
 import React from "react";
+import "../css/Home.css";
 
 function Home() {
-  return <h1>Home page</h1>;
+  return (
+    <>
+      <div className="home">
+        <a href="https://www.primevideo.com/?ref_=dvm_pds_amz_in_as_s_g_176_mkw_s4mEHKPKZ-dc&mrntrk=pcrid_386629063613_slid__pgrid_82649959567_pgeo_1007768_x__ptid_kwd-339065342343">
+          <img
+            className="home__image"
+            src="https://images-eu.ssl-images-amazon.com/images/G/02/digital/video/merch2016/Hero/Covid19/Generic/GWBleedingHero_ENG_COVIDUPDATE__XSite_1500x600_PV_en-GB._CB28684220_/jpg"
+            alt=""
+          />
+        </a>
+      </div>
+    </>
+  );
 }
 
 export default Home;
