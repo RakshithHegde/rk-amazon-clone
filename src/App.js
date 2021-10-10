3f21cd0f06bee47bdc75ce1f97cb8f35d9066210
Rakshith <rakshithhgd@gmail.com>
2021-10-10 19:55:32 +0530

    created amazon react app

--- src/App.js
+++ src/App.js
@@ -1,24 +1,31 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import React from "react";
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link,
+  useRouteMatch,
+  useParams,
+} from "react-router-dom";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div className="app">
+        <Switch>
+          <Route path="/checkout">
+            <h1>Checkout</h1>
+          </Route>
+          <Route path="/login-page">
+            <h1>Login-Page</h1>
+          </Route>
+          <Route path="/">
+            <h1>Home</h1> {/*Default page */}
+          </Route>
+        </Switch>
+      </div>
+    </Router>
   );
 }
 
---
+++ src/api/firebase.js
@@ -0,0 +1,10 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyDVPDeqL3FvvLwnz_w7KLYpV2NxfvWn-ew",
+  authDomain: "rk-a0037.firebaseapp.com",
+  projectId: "rk-a0037",
+  storageBucket: "rk-a0037.appspot.com",
+  messagingSenderId: "3576896679",
+  appId: "1:3576896679:web:660a8a33fc459c82bc4f82",
+  measurementId: "G-WZNGSTJGLW",
+};
