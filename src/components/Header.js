512f3a9973b7299e06f9b22e72cb3beca1574981
Rakshith <rakshithhgd@gmail.com>
2021-10-10 20:13:05 +0530

    header-created

---
+++ src/components/Header.js
@@ -0,0 +1,20 @@
+import React from "react";
+
+function Header() {
+  return (
+    <nav className="header">
+      {/*logo on left -> img */}
+      <img
+        className="header__logo"
+        src="http://pngimg.com/uploads/amazon/amazon_PNG11.png"
+        alt=""
+      />
+      {/*
+      search box
+      3 links(sign in,orders,prime)
+      basket or cart */}
+    </nav>
+  );
+}
+
+export default Header;
