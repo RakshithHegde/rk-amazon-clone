d8067f78ae267a4309d88b7e9b5c6901b0b98164
Rakshith <rakshithhgd@gmail.com>
2021-10-11 18:00:02 +0530

    product-init

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,5 +1,6 @@
 import React from "react";
 import "../css/Home.css";
+import Product from "./Product";
 
 function Home() {
   return (
@@ -12,6 +13,22 @@ function Home() {
             alt=""
           />
         </a>
+        {/*product id,title,proce,rating,image */}
+        <Product
+          id={12425466}
+          title="Microsoft - Xbox One S 1TB All-Digital Edition Console with Xbox One Wireless Controller"
+          price={499.99}
+          rating={4}
+          image="https://images-na.ssl-images-amazon.com/images/I/71XHuIVqJbL._AC_SX466_.jpg"
+        />
+        <Product
+          id={12425466}
+          title="HP Stream 14 inches HD(1366x768) Display, Intel Celeron N4000 Dual-Core Processor"
+          price={949.99}
+          rating={5}
+          image="https://images-na.ssl-images-amazon.com/images/I/81RCOr3hJOL._AC_SL1500_.jpg"
+        />
+        {/*product */}
       </div>
     </>
   );
---
+++ src/components/Product.js
@@ -0,0 +1,7 @@
+import React from "react";
+import "../css/Product.css";
+function Product({ id, title, image, proce, rating }) {
+  return <div></div>;
+}
+
+export default Product;
